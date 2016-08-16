d92c247663a27516a5fbe3f2158ae770023eea29
c4327611 <iii-user@iii-PC001>
2016-08-16 20:42:55 +0800

    year2

--- src/tw/jason/test1.java
+++ src/tw/jason/test1.java
@@ -3,7 +3,7 @@ package tw.jason;
 import  java.io.*;
 public class test1 {
 	public static void main(String[] args) throws IOException {
-		System.out.println("請輸入西元年份");
+		System.out.print("請輸入西元年份:");
 		
 		BufferedReader br = 
 				new BufferedReader(new InputStreamReader(System.in));
@@ -11,13 +11,13 @@ public class test1 {
 		 int year = Integer.parseInt(v1); //指定變數v1為int型態
 		 
 		 if (year % 400 ==0) {
-			 System.out.println("該年為閏年");
+			 System.out.println("西元" + year + "年為閏年");
 		 } else if (year % 100 == 0) {
-			 System.out.println("該年為平年");
+			 System.out.println("西元" + year + "年為平年");
 		 } else if (year % 4 == 0) {
-			 System.out.println("該年為閏年");
+			 System.out.println("西元" + year + "年為閏年");
 		 } else {
-			 System.out.println("該年為平年");
+			 System.out.println("西元" + year + "年為平年");
 		 }		
 	}
 
