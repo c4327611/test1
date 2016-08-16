d50d395d0f7006cf71dd52a0577439e73486ae3e
c4327611 <iii-user@iii-PC001>
2016-08-16 20:29:17 +0800

    year

--- src/tw/jason/test1.java
+++ src/tw/jason/test1.java
@@ -7,9 +7,8 @@ public class test1 {
 		
 		BufferedReader br = 
 				new BufferedReader(new InputStreamReader(System.in));
-		 String v1 = br.readLine();
-		 int year = Integer.parseInt(v1);
-		 System.out.println(year);
+		 String v1 = br.readLine(); //指定變數v1為使用者輸入值		 
+		 int year = Integer.parseInt(v1); //指定變數v1為int型態
 		 
 		 if (year % 400 ==0) {
 			 System.out.println("該年為閏年");
