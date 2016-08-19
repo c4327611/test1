3877b69fd68774a90bef5f0be11bf745d53cbf91
c4327611 <c4327611@gmail.com>
2016-08-19 22:20:29 +0800

    java.io Scanner double

--- src/tw/jason/test3.java
+++ src/tw/jason/test3.java
@@ -3,17 +3,17 @@ import java.io.*;
 public class test3 {
 
 	public static void main(String[] args) throws IOException {
-		// 3.7-4
+		// 3.7-4 
 		
 		System.out.println("請輸入身高和體重:");
-		BufferedReader br = 
+		BufferedReader bo = 
 				new BufferedReader(new InputStreamReader(System.in));
 		
-		String h1 = br.readLine();
-		String w1 = br.readLine();
+		String h1 = bo.readLine();
+		String w1 = bo.readLine();
 		
-		double h2 = Integer.parseInt(h1);
-		double w2 = Integer.parseInt(w1);
+		double h2 = Double.parseDouble(h1);
+		double w2 = Double.parseDouble(w1);
 		System.out.println("您的身高是" + h2 + "公分 。");
 		System.out.println("您的體重是" + w2 + "公斤。");
 		double h3 = h2/100;
