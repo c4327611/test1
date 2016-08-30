9e9c389cbe955b2d3c044c9592d46c2658194b23
renting <asdf11191030@gmail.com>
2016-08-30 21:19:26 +0800

    pm

--- src/tw/jason/Guess2.java
+++ src/tw/jason/Guess2.java
@@ -1,26 +1,54 @@
 package tw.jason;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class Guess2 extends JFrame {
+public class Guess2 extends JFrame  {
 		// 
 		private JButton Guess;
 		private JTextField Intput;
 		private JTextArea Output;
+		
 			
-		
-		
 		public Guess2(){
 			super("猜數字");
+			
 			Guess = new JButton("猜");
-			Intput = new JTextField(); //輸入數字
+			Intput = new JTextField(35);
 			Output = new JTextArea(); //輸出+XAXB
 			
-		}
+			setLayout(new BorderLayout()); 
+			
+			JPanel top = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-	}
+			top.add(Intput);top.add(Guess);
+			
+			add(top,BorderLayout.NORTH);
+			add(Output, BorderLayout.CENTER); //中間
+			
+			
+			setSize(640, 480); //設定大小
+			setVisible(true); //顯示
+			setDefaultCloseOperation(EXIT_ON_CLOSE); //結束視窗.定義在JFram
+		}
+		
+			public static void main(String[] args) {
+				new Guess2();
+					
+
+					
+				}
+				
+				
+			}
+			
+		
+
 
 
