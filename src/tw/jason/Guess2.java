afb90b3fb447e5a64a6d9f5adc4ed414cf38dfcf
c4327611 <c4327611@gmail.com>
2016-08-31 22:36:26 +0800

    0831

--- src/tw/jason/Guess2.java
+++ src/tw/jason/Guess2.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -14,7 +15,7 @@ public class Guess2 extends JFrame  {
 		private JButton Guess;
 		private JTextField Intput;
 		private JTextArea Output;
-		
+		String answer = createAnswer(4);
 			
 		public Guess2(){
 			super("猜數字");
@@ -36,7 +37,83 @@ public class Guess2 extends JFrame  {
 			setSize(640, 480); //設定大小
 			setVisible(true); //顯示
 			setDefaultCloseOperation(EXIT_ON_CLOSE); //結束視窗.定義在JFram
+		
+			boolean isWINNER = false;
+			for (int i=0; i<10; i++){
+				// 開始猜
+				
+				
+				// 比對
+				if (Intput.equals(answer)){
+					// 對了 => 恭喜老爺,...
+					isWINNER = true;
+					JOptionPane.showMessageDialog(null, "恭喜老爺,...");
+					break;
+				}else{
+					// 錯了 => 提示 1A2b
+					String result = checkAB(answer, Intput);
+					JOptionPane.showMessageDialog(null, Intput + ":" + result);
+				}
+			}
+			if (!isWINNER){
+				JOptionPane.showMessageDialog(null, "嗐, 魯蛇, \n答案是:" + 
+							answer);
+			}
+				
+			
+			
 		}
+			
+		private String checkAB(String answer, JTextField intput) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		static String checkAB(String a, String g){
+			int A, B; A = B = 0;
+			for (int i=0; i<g.length();i++){
+				if (g.charAt(i) == a.charAt(i)){
+					A++;
+				}else if (a.indexOf(g.charAt(i)) != -1){
+					B++;
+				}
+			}
+			return A + "A" + B + "B";
+		}
+		
+		static String createAnswer(int n){
+			// 洗牌
+			int[] poker = new int[n];	// 0,0,...0
+			int rand;
+			boolean isOK;
+			for (int i=0; i<poker.length; i++){
+				
+				do {
+					rand = (int)(Math.random()*10);
+					// 檢查機制
+					isOK = true;
+					for (int j=0;j<i;j++){
+						if (poker[j]==rand){
+							isOK = false;
+							break;
+						}
+					}
+					
+				}while (!isOK);
+				
+				poker[i] = rand;
+				//System.out.println(poker[i]);
+			}
+			String ret = "";
+			for (int v : poker) ret += v;
+			
+			return ret;
+		}
+		
+	
+
+		
+		
 		
 			public static void main(String[] args) {
 				new Guess2();
@@ -44,6 +121,7 @@ public class Guess2 extends JFrame  {
 
 					
 				}
+			
 				
 				
 			}
@@ -52,3 +130,4 @@ public class Guess2 extends JFrame  {
 
 
 
+//http://pydoing.blogspot.tw/2011/05/java-basic-concept-of-gui.html
