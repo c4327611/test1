cc52575f7d6a9ae0b6f49df9530c91349f579126
renting <asdf11191030@gmail.com>
2016-09-01 21:19:41 +0800

    0901pm

--- src/tw/jason/Lotto.java
+++ src/tw/jason/Lotto.java
@@ -1,29 +1,43 @@
-package tw.jason; // 樂透號碼產生
+package tw.jason;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class Lotto extends JFrame{
+public class Lotto extends JFrame {
+	
 	private JButton select;
-	private JLabel Output;
+	private JLabel Output,Output1,Output2,Output3,Output4,Output5;
 	
 		public Lotto(){
 		
-		JFrame demo = new JFrame("樂透號碼產生器");
+		super("樂透號碼產生器");
 		select = new JButton("49選6");
-		Output = new JLabel("怎麼輸出?");
+		Output = new JLabel("輸出");
+		Output1 = new JLabel("輸出");
+		Output2 = new JLabel("輸出");
+		Output3 = new JLabel("輸出");
+		Output4 = new JLabel("輸出");
+		Output5 = new JLabel("輸出");
 		
-		setSize(640, 480); //設定大小
+		setSize(480, 320); //設定大小
 		setVisible(true); //顯示
 		setDefaultCloseOperation(EXIT_ON_CLOSE); //結束視窗.定義在JFram
         
         setLayout(new BorderLayout());
         add(select,BorderLayout.NORTH);
-		add(Output, BorderLayout.CENTER);
+		add(Output,BorderLayout.CENTER);
+		
+		select.setFont(new Font("標楷體", Font.BOLD, 28));
+		Output.setFont(new Font("標楷體", Font.BOLD, 25));
 		}
+
+
+	
 //        int[] num = new int[6];
 //        int rand;
 //		boolean isOK;
@@ -44,10 +58,13 @@ public class Lotto extends JFrame{
 //			}while (!isOK);
 //			
 //			num[i] = rand;
+//		    System.out.println(num[i]);
 //        }
 //	}
-        public static void main(String[] args){
-        new Lotto();
+		public static void main(String[] args) {
+			new Lotto();
+
+		}
 }
-}
+
 //http://yhhuang1966.blogspot.tw/2014/02/java-swing-jlabel.html
